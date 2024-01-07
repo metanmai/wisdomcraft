02d88802cc190b2330db9ed949fddfcb1e667e51
metanmai <metanmai@icloud.com>
2024-01-07 10:33:36 +0530

    Added Navbar Routes.

--- app/(dashboard)/_components/navbar.tsx
+++ app/(dashboard)/_components/navbar.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import MobileSidebar from "@/app/(dashboard)/_components/mobile-sidebar";
+import NavbarRoutes from "@/components/navbar-routes";
 
 const Navbar = () => {
 	return (
 		<div className={`p-4 border-b h-full flex items-center bg-white shadow-sm`}>
 			<MobileSidebar/>
+			<NavbarRoutes/>
 		</div>
 	);
 };
