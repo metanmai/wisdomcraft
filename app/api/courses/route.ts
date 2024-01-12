14bbff4b852032756fe80db4e86d01f2552c7799
metanmai <metanmai@icloud.com>
2024-01-12 17:15:01 +0530

    Removed router.push statement.

--- app/api/courses/route.ts
+++ app/api/courses/route.ts
@@ -2,7 +2,7 @@ import {NextResponse} from "next/server";
 import {auth} from "@clerk/nextjs";
 import {db} from "@/lib/db";
 
-export async function POST(req: Request, res: Response) {
+export async function POST(req: Request) {
 	try {
 		const {userId} = auth();
 		const {title} = await req.json();
